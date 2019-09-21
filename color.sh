384021ed888d7c16a5404ad6aceb16eaabb6d6e3
nuthanc <nuthanc@juniper.net>
2019-09-22 00:50:28 +0530

    Cat resolv and reimage

---
+++ color.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+ssh-keygen -R nodeg12
+ssh-keygen -R nodeg31
+ssh-keygen -R nodec58
+ssh-keygen -R nodec60
+ssh-keygen -R nodec61
+ssh-keygen -R nodea4
+
+ansible-playbook -i inventory playbooks/color_ps.yml 
+
---
+++ run-playbook.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+ssh-keygen -R nodeg12
+ssh-keygen -R nodeg31
+ssh-keygen -R nodec58
+ssh-keygen -R nodec60
+ssh-keygen -R nodec61
+
+ansible-playbook -i inventory playbooks/cat_resolv.yml 
+
