3570082ba2ed0215efb49e1d6a0861a29a82175f
Nuthan <52997725+nuthanc@users.noreply.github.com>
2020-09-10 00:04:46 +0530

    Fix for cases involving @skip decorator

--- playbooks/roles/sanity/files/add_rerun_tag.py
+++ playbooks/roles/sanity/files/add_rerun_tag.py
@@ -10,8 +10,13 @@ def parse(method_name, file, tag='rerun'):
   with open(file, 'r') as f:
     config_file = f.read()
 
-  pattern = r"(@test.attr\(type=\[)(.*)(\]\W+.*\W+def " + method_name +")"
-  tagged_code = re.sub(pattern, rf"\1\2, '{tag}'\3", config_file, re.M)
+  pattern = rf"(@test.attr\(type=\[)(.*)(\]\W+.*\W+def) ({method_name})"
+  found_code = re.search(pattern, config_file, re.M)
+  # If code is not found, it is due to skip decorator,so find it in next line
+  if not found_code:
+    pattern = rf"(@test.attr\(type=\[)(.*)(\]\W+.*\W+.*\W+def) ({method_name})"
+  # Group 2 is the existing tag and Group 4 is the method_name  
+  tagged_code = re.sub(pattern, rf"\1\2, '{tag}'\3\4", config_file, re.M)
   if tagged_code:
     with open(file, 'w') as f:
       f.write(tagged_code)
