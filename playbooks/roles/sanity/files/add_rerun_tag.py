f8065ac7be7a86588bd1085ee03ff55ce8e3e727
Nuthan <52997725+nuthanc@users.noreply.github.com>
2020-09-10 00:14:40 +0530

    Add space in pattern to fix space issue after def

--- playbooks/roles/sanity/files/add_rerun_tag.py
+++ playbooks/roles/sanity/files/add_rerun_tag.py
@@ -10,11 +10,11 @@ def parse(method_name, file, tag='rerun'):
   with open(file, 'r') as f:
     config_file = f.read()
 
-  pattern = rf"(@test.attr\(type=\[)(.*)(\]\W+.*\W+def) ({method_name})"
+  pattern = rf"(@test.attr\(type=\[)(.*)(\]\W+.*\W+def )({method_name})"
   found_code = re.search(pattern, config_file, re.M)
   # If code is not found, it is due to skip decorator,so find it in next line
   if not found_code:
-    pattern = rf"(@test.attr\(type=\[)(.*)(\]\W+.*\W+.*\W+def) ({method_name})"
+    pattern = rf"(@test.attr\(type=\[)(.*)(\]\W+.*\W+.*\W+def )({method_name})"
   # Group 2 is the existing tag and Group 4 is the method_name  
   tagged_code = re.sub(pattern, rf"\1\2, '{tag}'\3\4", config_file, re.M)
   if tagged_code:
@@ -25,4 +25,4 @@ def parse(method_name, file, tag='rerun'):
 failure_tc.parse_data()
 
 for i, tc in enumerate(failure_tc.tc_list):
-  parse(tc, failure_tc.loc_list[i])
+  parse(method_name=tc, file=failure_tc.loc_list[i])
