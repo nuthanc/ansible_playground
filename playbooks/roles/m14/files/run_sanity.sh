d9a306aebd66445799e51484372a5436e3f5048e
nuthanc <nuthanc@juniper.net>
2021-01-08 12:05:03 +0530

    Add m14 script

---
+++ playbooks/roles/m14/files/run_sanity.sh
@@ -0,0 +1,11 @@
+cat <<EOF> env.list
+PYTHONPATH=./scripts:./fixtures
+TEST_CONFIG_FILE=contrail_test_input.yaml
+EMAIL_SUBJECT=Python3-sanity:k8s
+MX_GW_TEST=1
+PYTHON3=1
+EOF
+
+docker run --network=host -t --env-file ./env.list \
+-v /root/contrail_test_input.yaml:/contrail-test/contrail_test_input.yaml \
+bng-artifactory.juniper.net/contrail-nightly/contrail-test-test:$1 -c -x /contrail-test/run_tests.sh -m -U -T sanity -t
