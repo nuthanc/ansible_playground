3b324cd150ade6135922f58997c2976d03ad22f4
nuthanc <nuthanc@juniper.net>
2021-02-26 00:20:05 -0800

    Update cp sanity files

--- playbooks/roles/n18/files/run_sanity.sh
+++ playbooks/roles/n18/files/run_sanity.sh
@@ -1,3 +1,3 @@
-version=$(docker ps|grep config-api|awk '{print $2}'|cut -d : -f2)
+version=$(grep '&image-tag' /root/CEM/13066/my-k8s-os-mi.yaml|awk '{print $NF}'|tr -d '"')
 
 docker run --name nuthan_test --entrypoint /bin/bash --env-file /root/env_file -v /root/contrail_test_input.yaml:/contrail-test/contrail_test_input.yaml -v /etc/contrail:/etc/contrail -v /root/.ssh:/root/.ssh --network=host -it bng-artifactory.juniper.net/contrail-nightly/contrail-test-test:${version}
