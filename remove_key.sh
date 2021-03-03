abdfde9d70195e0b52f5549af3b7a3e54cd32bb2
Nuthan Chandra <nuthanc@juniper.net>
2021-03-03 15:46:39 +0530

    Add flow role

--- remove_key.sh
+++ remove_key.sh
@@ -13,3 +13,9 @@ ssh-keygen -R 10.204.218.105
 ssh-keygen -R 10.204.217.130
 ssh-keygen -R 10.204.217.6
 ssh-keygen -R 10.204.217.4
+ssh-keygen -R nodem8.englab.juniper.net
+ssh-keygen -R nodem9.englab.juniper.net
+ssh-keygen -R nodem10.englab.juniper.net
+ssh-keygen -R nodem6.englab.juniper.net
+ssh-keygen -R nodem14.englab.juniper.net
+ssh-keygen -R nodem7.englab.juniper.net
