d788e51fbcdfd870f5386f33c6088cc4570bd7d8
Nuthan Chandra <nuthanc@juniper.net>
2021-03-26 11:39:39 +0530

    Update run_sanity

--- playbooks/roles/m14/files/run_sanity.sh
+++ playbooks/roles/m14/files/run_sanity.sh
@@ -1,1 +1,3 @@
-docker run --name nuthan_test --entrypoint /bin/bash --env-file /root/env_file -v /root/contrail_test_input.yaml:/contrail-test/contrail_test_input.yaml -v /root/my_script.sh:/my_script.sh --network=host -it bng-artifactory.juniper.net/contrail-nightly/contrail-test-test:master.1544
+version=$(docker ps|grep control_control_1|awk '{print $2}'|cut -d : -f 2|cut -d - -f 1)
+
+docker run --name nuthan_test --entrypoint /bin/bash --env-file /root/env_file -v /root/contrail_test_input.yaml:/contrail-test/contrail_test_input.yaml -v /root/my_script.sh:/my_script.sh --network=host -it bng-artifactory.juniper.net/contrail-nightly/contrail-test-test:${version}
