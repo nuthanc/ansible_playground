c71b5ea1bdc9b72b0b5c8ffa259a36bf8e16fcba
Nuthan Chandra <nuthanc@juniper.net>
2021-07-13 12:11:53 +0530

    Update

--- playbooks/roles/phy_sanity/files/login_tc.sh
+++ playbooks/roles/phy_sanity/files/login_tc.sh
@@ -1,3 +1,3 @@
-version=$(docker ps|grep control_control_1|awk '{print $2}'|cut -d : -f 2|cut -d - -f 1)
+version=$(docker ps|grep control_control_1|awk '{print $2}'|cut -d : -f 2)
 
 docker run --name nuthan_test --entrypoint /bin/bash --env-file /root/env_file -v /root/contrail_test_input.yaml:/contrail-test/contrail_test_input.yaml --network=host -it bng-artifactory.juniper.net/contrail-nightly/contrail-test-test:${version}
